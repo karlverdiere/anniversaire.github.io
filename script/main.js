6ff5be155df284f919773ad00cbd621fa7eb89b0
karl verdiere <karlverdiere@macbook-pro-de-karl.home>
2018-09-28 17:42:59 +0200

    cdsdebutcode

---
+++ script/main.js
@@ -0,0 +1,17 @@
+function directxxX()
+{
+	if (confirm('joyeux anniversaire raphael <3'))
+	{
+		document.location.href = 'https://www.youtube.com/watch?v=L0cBDXfcObI';
+	}
+};
+function anniversaire()
+{
+	if (confirm('joyeux anniversaire balou <3'))
+	{
+		document.location.href = 'https://www.youtube.com/watch?v=HKpo7RA15yY ';
+	}
+};
+
+setTimeout(directxxX, 10000);
+setTimeout(anniversaire, 20000);
